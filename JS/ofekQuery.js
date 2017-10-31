4decc66c519fe61eb11bb458b8cf5c5c8cb71a22
Jbt <5770yr@gmail.com>
2017-10-31 09:43:51 +0200

    initial commit

---
+++ JS/ofekQuery.js
@@ -0,0 +1,15 @@
+function $(query) {
+	return new ofekQuery(query);
+}
+
+var ofekQuery = function (query) {
+	this.arrayElemens = [];
+	this.dadada = function () {
+
+		var selectorCollection;
+
+		selectorCollection = document.getElementById();
+	};
+
+
+};
